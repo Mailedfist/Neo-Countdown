f07c07ba9441b3b539bc9e62756ad50ce619a506
Mailedfist <32024311+Mailedfist@users.noreply.github.com>
2017-09-16 23:26:11 +0100

    Add files via upload
    added ino file

---
+++ NeopixelCountdown.ino
@@ -0,0 +1,44 @@
+#include <Adafruit_NeoPixel.h>
+  //NEOPIXEL INT
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN            1
+#define NUMPIXELS      8
+
+//initiate variables
+int daysLeft = 255;
+int pixel_number = 0;
+int red = 255;
+int green = 255;
+int blue = 255;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // Neopixel setup
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  strip.begin(); // This initializes the NeoPixel library.
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    strip.setPixelColor(pixel_number, strip.Color(red, green, blue)); // 
+    strip.show(); // This sends the updated pixel color to the hardware.
+ //for daysLeft > 0
+ // set timer for 24h   
+ //when timer expires, change color of neopixel
+
+ delay (86400000);
+ // change pixel colour values
+
+ 
+}
