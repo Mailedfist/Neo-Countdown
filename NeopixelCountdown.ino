7dc54285c21dc55fb5e646e6acf3f140036d8bf2
Mailedfist <32024311+Mailedfist@users.noreply.github.com>
2017-09-17 21:39:09 +0100

    Moved to FastLED library

--- NeopixelCountdown.ino
+++ NeopixelCountdown.ino
@@ -1,10 +1,8 @@
-#include <Adafruit_NeoPixel.h>
-  //NEOPIXEL INT
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-#define PIN            5 //pin 5 for testing on ardiuno uno
-#define NUMPIXELS      8
+#include <FastLED.h>
+
+  //FASTLED INT
+#define DATA_PIN            5 //pin 5 for testing on ardiuno uno
+#define NUM_LEDS     8
 
 //initiate variables
 int daysLeft = 255;
@@ -13,28 +11,19 @@ int pixel_number = 0;
 int red = 255;
 int green = 255;
 int blue = 255;
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  // Neopixel setup
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
-  strip.begin(); // This initializes the NeoPixel library.
-  strip.setBrightness(30); // Reduce the brightness of the strip
-  strip.show(); // Initialize all pixels to 'off'
+  // Neopixel (FastLED) setup
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(pixel_number, strip.Color(red, green, blue)); // 
-    strip.show(); // This sends the updated pixel color to the hardware.
+    leds[pixel_number].setRGB(red, green, blue); // 
+    FastLED.show(); // This sends the updated pixel color to the hardware.
  //for daysLeft > 0
  // set timer for 24h   
  //when timer expires, change color of neopixel
